8de4f19b24caed1ca409925a93fef2625c84fe87
Jeremie Dimino <jdimino@janestreet.com>
2018-02-13 14:23:25 +0000

    checkdoc

--- line-up-words.el
+++ line-up-words.el
@@ -15,7 +15,7 @@
 
 ;;;###autoload
 (defun line-up-words ()
-  "Tries to align words in the region in an intelligent way."
+  "Try to align words in the region in an intelligent way."
   (interactive)
   (shell-command-on-region
    (region-beginning) (region-end) line-up-words-command t t
